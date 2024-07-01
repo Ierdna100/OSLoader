5ac5068281005ca08b25f9b6b82b867c505a3f4f
Andrei <ierdna100@yahoo.com>
2024-06-30 20:14:42 -0400

    Renamed API name

--- OSLoader/OSLoader/OSAPI.cs
+++ OSLoader/OSLoader/OSAPI.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using UnityEngine.Events;
+using Newtonsoft.Json;
 
 namespace OSLoader
 {
-    public static class ModAPI
+    public static class OSAPI
     {
-        public static string ConfigFilePath { get; private set; }
-
+        
     }
 }
 
