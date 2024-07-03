fffed551dc58390830865c06973fc88594675d2d
Andrei <ierdna100@yahoo.com>
2024-07-03 02:52:06 -0400

    Restructured mod structure

--- OSLoader/OSLoader/Mod/Mod.cs
+++ OSLoader/OSLoader/Mod/Mod.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using System.IO;
 using UnityEngine;
 
 namespace OSLoader
 {
     public class Mod : MonoBehaviour
     {
-        public ModConfig config;
-        protected Logger logger;
+        public ModInfo info;
+        public ModSettings settings;
+        public Logger logger;
 
         public virtual void OnModLoaded()
         {
-            logger = new Logger(config.name);
+            logger = new Logger(info.name);
+        }
+
+        public void SaveSettings()
+        {
+            if (settings?.settings == null) return;
+            File.WriteAllText(info.settingsFilepath, JsonConvert.SerializeObject(settings, Formatting.Indented));
         }
     }
 }
