11d38f3e1c1dc73fb33a0aa499f8d69ee5955b25
Andrei <ierdna100@yahoo.com>
2024-07-03 02:52:43 -0400

    Added setting system

---
+++ OSLoader/OSLoader/Mod/ModSettingAttribute.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLoader
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public abstract class ModSettingAttribute : Attribute
+    {
+        internal string name;
+
+        public ModSettingAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class SettingTitleAttribute : Attribute
+    {
+        internal string name;
+
+        public SettingTitleAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class StringSettingAttribute : ModSettingAttribute
+    {
+        internal string defaultValue;
+        internal uint maxLength = uint.MaxValue;
+        internal StringConstraints constraints;
+
+        public StringSettingAttribute(string name, string defaultValue) : base(name)
+        {
+            this.defaultValue = defaultValue;
+        }
+
+        public StringSettingAttribute(string name, string defaultValue, StringConstraints constraints) : base(name)
+        {
+            this.defaultValue = defaultValue;
+            this.constraints = constraints;
+        }
+
+        public StringSettingAttribute(string name, string defaultValue, uint maxLength) : base(name)
+        {
+            this.defaultValue = defaultValue;
+            this.maxLength = maxLength;
+        }
+
+        public StringSettingAttribute(string name, string defaultValue, StringConstraints constraints, uint maxLength) : base(name)
+        {
+            this.defaultValue = defaultValue;
+            this.constraints = constraints;
+            this.maxLength = maxLength;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class IntegerSettingAttribute : ModSettingAttribute
+    {
+        internal int defaultValue;
+        internal int maxValue;
+        internal int minValue;
+        internal int step;
+
+        public IntegerSettingAttribute(string name, int defaultValue, int minValue, int maxValue, int step) : base(name)
+        {
+            this.defaultValue = defaultValue;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.step = step;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class FloatSettingAttribute : ModSettingAttribute
+    {
+        internal float defaultValue;
+        internal float maxValue;
+        internal float minValue;
+        internal float step;
+
+        public FloatSettingAttribute(string name, float defaultValue, float minValue, float maxValue, float step) : base(name)
+        {
+            this.defaultValue = defaultValue;
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.step = step;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class BoolSettingAttribute : ModSettingAttribute
+    {
+        internal bool defaultValue;
+
+        public BoolSettingAttribute(string name, bool defaultValue) : base(name)
+        {
+            this.defaultValue = defaultValue;
+        }
+    }
+}
---
+++ OSLoader/OSLoader/Mod/ModSettings.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace OSLoader
+{
+    public abstract class ModSettings
+    {
+        // String indicates a title that should go above it
+        internal List<Tuple<FieldInfo, string>> settings = new List<Tuple<FieldInfo, string>>();
+
+        public ModSettings()
+        {
+            foreach (FieldInfo fieldInfo in GetType().GetFields())
+            {
+                ModSettingAttribute attribute = fieldInfo.GetCustomAttribute<ModSettingAttribute>();
+                if (attribute == null)
+                {
+                    Loader.Instance.logger.Error("A setting in the mod settings lacks an attribute! Cannot generate settings.");
+                    settings = null;
+                    return;
+                }
+
+                if (attribute is StringSettingAttribute && fieldInfo.FieldType != typeof(string)
+                    || attribute is BoolSettingAttribute && fieldInfo.FieldType != typeof(bool)
+                    || attribute is FloatSettingAttribute && fieldInfo.FieldType != typeof(float)
+                    || attribute is IntegerSettingAttribute && fieldInfo.FieldType != typeof(int))
+                {
+                    Loader.Instance.logger.Error("Type mismatch in settings attributes! Cannot generate settings menu.");
+                    settings = null;
+                    return;
+                }
+
+                SettingTitleAttribute title = fieldInfo.GetCustomAttribute<SettingTitleAttribute>();
+                settings.Add(new Tuple<FieldInfo, string>(fieldInfo, title?.name));
+            }
+
+            GenerateDefaultSettings();
+        }
+
+        private void GenerateDefaultSettings()
+        {
+            if (settings == null) return;
+
+            foreach (Tuple<FieldInfo, string> tuple in settings)
+            {
+                ModSettingAttribute attribute = tuple.Item1.GetCustomAttribute<ModSettingAttribute>();
+                if (attribute is StringSettingAttribute stringAttribute)
+                    tuple.Item1.SetValue(this, stringAttribute.defaultValue);
+                if (attribute is BoolSettingAttribute boolAttribute)
+                    tuple.Item1.SetValue(this, boolAttribute.defaultValue);
+                if (attribute is IntegerSettingAttribute intAttribute)
+                    tuple.Item1.SetValue(this, intAttribute.defaultValue);
+                if (attribute is FloatSettingAttribute floatAttribute)
+                    tuple.Item1.SetValue(this, floatAttribute.defaultValue);
+            }
+        }
+    }
+}
