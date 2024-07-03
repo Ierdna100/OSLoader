3416787b03e5b035613c6ea04507e36679378449
Andrei <ierdna100@yahoo.com>
2024-07-03 02:52:19 -0400

    Renamed ModConfig to ModInfo

---
+++ OSLoader/OSLoader/Mod/ModInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace OSLoader
+{
+    public class ModInfo
+    {
+        [JsonIgnore]
+        internal string infoFilepath;
+        [JsonIgnore]
+        internal string settingsFilepath;
+
+        public string name;
+        public string description;
+        public bool loadOnStart;
+        public string version;
+
+        public bool IsValid()
+        {
+            return name != null && version != null;
+        }
+
+        public void SaveInfo()
+        {
+            File.WriteAllText(infoFilepath, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+    }
+}
