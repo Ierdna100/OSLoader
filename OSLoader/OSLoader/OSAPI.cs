1b7234cf37d50bd92d183a7b1d6271c8e705ab74
Andrei <ierdna100@yahoo.com>
2024-07-05 03:54:31 -0400

    Added some API functionality

--- OSLoader/OSLoader/OSAPI.cs
+++ OSLoader/OSLoader/OSAPI.cs
@@ -4,12 +4,23 @@ using System.Text;
 using System.IO;
 using UnityEngine.Events;
 using Newtonsoft.Json;
+using UnityEngine;
 
 namespace OSLoader
 {
     public static class OSAPI
     {
-        
+        public static Version GameVersion
+        {
+            get { return new Version(Application.version); }
+            private set { }
+        }
+
+        public static OSScene CurrentScene
+        {
+            get { return Loader.Instance.currentScene; }
+            private set { }
+        }
     }
 }
 
