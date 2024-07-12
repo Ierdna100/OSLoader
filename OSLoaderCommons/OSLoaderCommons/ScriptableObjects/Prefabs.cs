5a58c93d9dc0bd5315d94022a31d53ee596bf8f5
Andrei Popescu <ierdna100@yahoo.com>
2024-07-12 15:28:45 -0400

    Fixed spacing in Prefabs SO

--- OSLoaderCommons/OSLoaderCommons/ScriptableObjects/Prefabs.cs
+++ OSLoaderCommons/OSLoaderCommons/ScriptableObjects/Prefabs.cs
@@ -8,14 +8,17 @@ namespace OSLoader
     [CreateAssetMenu(fileName = "Prefabs")]
     internal class Prefabs : ScriptableObject
     {
+        public GameObject canvas;
         public GameObject loaderMenu;
         public GameObject modEntry;
+
         public GameObject settingHeader;
+
         public GameObject stringSetting;
         public GameObject intSetting;
-        public GameObject floatSetting;
-        public GameObject boolSetting;
         public GameObject intSliderSetting;
+        public GameObject floatSetting;
         public GameObject floatSliderSetting;
+        public GameObject boolSetting;
     }
 }
