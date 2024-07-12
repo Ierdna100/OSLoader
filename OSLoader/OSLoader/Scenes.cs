8ab5c19209d02dc56df0218696cdaaffce544ce1
Andrei Popescu <ierdna100@yahoo.com>
2024-07-12 08:17:51 -0400

    Added scenes

--- OSLoader/OSLoader/Scenes.cs
+++ OSLoader/OSLoader/Scenes.cs
@@ -6,6 +6,39 @@ namespace OSLoader
 {
     public enum OSScene
     {
-
+        MainMenu,
+        OpenSewer_Intro,
+        OpenSewer_Tenement,
+        Interior_Start,
+        Interior_PlayerTenement,
+        InteriorTenement_PlayerCanalSaunas,
+        InteriorTenement_B,
+        InteriorTenement_DeekulaA,
+        InteriorTenement_DeekulaB,
+        InteriorTenement_DeekulaC,
+        InteriorTenement_DeekulaMineEntrance,
+        InteriorTenement_GeneralStore,
+        InteriorTenement_OMarket,
+        InteriorTenement_Pharmacy,
+        InteriorTenement_Speakeasy,
+        InteriorTenement_Greenhouse,
+        InteriorTenement_GreenhouseStorage,
+        InteriorTenement_KurahaaraHome,
+        InteriorTenement_Gatehouse,
+        InteriorTenement_House3,
+        InteriorTenement_House4,
+        InteriorTenement_House5,
+        InteriorTenement_LotShack1,
+        InteriorTenement_LotShack2,
+        InteriorTenement_LotShack3,
+        InteriorTenement_RedemptionMilitia,
+        InteriorTenement_Bus,
+        InteriorTenement_Kolhola,
+        InteriorTenement_Caravan,
+        UnderMap,
+        TestMapEmpty,
+        TestMapCharacters,
+        OpenSewer_Mines,
+        InteriorTenement_A
     }
 }
