688b1bc357d28070070bcbf48df8782e9ef4e1d9
Andrei <ierdna100@yahoo.com>
2024-09-30 03:52:54 -0400

    Fixed various bugs in ProjectReference

---
+++ ProjectReferences/Assets/AssetBundleDebugger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OSLoader;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+public class AssetBundleDebugger : MonoBehaviour
+{
+    public MenuPrefabs menuPrefabs;
+
+    [ContextMenu("WOOOOOOO")]
+    private void LoadAssetBundle()
+    {
+        var assetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "loader"));
+        menuPrefabs = assetBundle.LoadAllAssets<MenuPrefabs>().SingleOrDefault();
+
+        Instantiate(menuPrefabs.modEntry);
+    }
+}
