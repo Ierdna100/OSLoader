8ad2167871274106e4b0fa1fd3debf1619dbed06
Andrei <ierdna100@yahoo.com>
2024-09-30 03:39:31 -0400

    KeybindConstraints were slightly modified to remove functionality that will not be implemented

--- OSLoader/OSLoader/Settings/KeybindConstraints.cs
+++ OSLoader/OSLoader/Settings/KeybindConstraints.cs
@@ -11,12 +11,12 @@ namespace OSLoader
         Default =               0b0010010,
 
         NoFunctions =           0b0000001,
-        NoEscape =              0b0000010,
+        //NoEscape =              0b0000010,
         NoExistingGameBinds =   0b0000100,
         NoExistingModBinds =    0b0001000,
         NoDefaultSteamBinds =   0b0010000,
-        NoDoubleClicks =        0b0100000,
-        NoTripleClicks =        0b1000000,
+        //NoDoubleClicks =        0b0100000,
+        //NoTripleClicks =        0b1000000,
 
         NoExistingBinds =       0b0011100
     }
